c834e3c085004bbd3eb959ee5c2b9bc477042c76
iQuadCore <33097095+iQuadCore@users.noreply.github.com>
2020-09-29 23:30:27 +0300

    Load crc32 lib instead of sha1

--- __resource.lua
+++ __resource.lua
@@ -1,14 +1,14 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 server_scripts {
-	"sha1.lua",
+	"crc32.lua",
 	"settings.lua",
 	"servierer.lua"
 }
 
 
 client_scripts {
-	"sha1.lua",
+	"crc32.lua",
 	"settings.lua",
 	"klient.lua"
 }
