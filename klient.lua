740e872bbc6e67edae4878637f5057c682aa275b
iQuadCore <33097095+iQuadCore@users.noreply.github.com>
2020-09-29 23:06:46 +0300

    Update klient.lua
    Use crc32 instead of sha1

--- klient.lua
+++ klient.lua
@@ -2,7 +2,7 @@ local hashtable = {}
 Citizen.CreateThread(function() -- hash generation thread
 	Wait(5000)
 	for i,a in ipairs(FilesHashesToVerify) do
-		local hash = sha1(LoadResourceFile(a.resource, a.file))
+		local hash = crc32(LoadResourceFile(a.resource, a.file))
 		table.insert(hashtable, {resource = a.resource,file = a.file, hash = hash})
 		if i == #FilesHashesToVerify then
 			TriggerServerEvent("VerifyFileHash", hashtable)
